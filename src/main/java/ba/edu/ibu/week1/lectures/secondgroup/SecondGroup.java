315e7f9168822619967886f7c09cf3e74bb7e91f
ibecir <becir@bioptimizers.com>
2024-10-02 10:00:56 +0200

    week 1 materials

---
+++ src/main/java/ba/edu/ibu/week1/lectures/secondgroup/SecondGroup.java
@@ -0,0 +1,33 @@
+package ba.edu.ibu.week1.lectures.secondgroup;
+
+import java.util.Scanner;
+
+public class SecondGroup {
+    public static void main(String[] args) {
+        simpleCalculator();
+    }
+    public static void simpleCalculator(){
+        while (true){
+            System.out.println("Please choice one option: sum / multiply / quit");
+            Scanner scanner = new Scanner(System.in);
+            String command = scanner.nextLine();
+
+            if(command.equals("quit"))
+                break;
+
+            if(command.equals("sum") || command.equals("multiply")){
+                System.out.println("Please enter first number: ");
+                int firstNumber = Integer.parseInt(scanner.nextLine());
+
+                System.out.println("Please enter second number: ");
+                int secondNumber = Integer.parseInt(scanner.nextLine());
+
+                if(command.equals("sum"))
+                    System.out.println("Sum of provided numbers is " + (firstNumber + secondNumber));
+                else
+                    System.out.println("Multiplication of provided numbers is " + (firstNumber * secondNumber));
+            } else
+                System.out.println("Invalid command `" + command + "`");
+        }
+    }
+}
