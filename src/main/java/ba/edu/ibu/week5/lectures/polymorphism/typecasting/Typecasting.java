8b3d04ad84dc238623f31b58108bfaa19d0a1afb
ibecir <becir@bioptimizers.com>
2024-10-11 09:30:02 +0200

    Week 5 materials

--- src/main/java/ba/edu/ibu/week5/lectures/polymorphism/typecasting/Typecasting.java
+++ src/main/java/ba/edu/ibu/week5/lectures/polymorphism/typecasting/Typecasting.java
@@ -1,13 +1,39 @@
 package ba.edu.ibu.week5.lectures.polymorphism.typecasting;
 
-class Animal {}
+class Animal {
+    public static void eat() {
+        System.out.println("Animal is eating");
+    }
+}
 
-class Turtle extends Animal {}
+class Turtle extends Animal { }
 
-class Tiger extends Animal {}
+class Tiger extends Animal {
+    public static void eatMyLittleTiger() {
+        System.out.println("My little tiger is eating");
+    }
+}
 
 class Main {
     public static void main(String[] args) {
         Animal animal = new Tiger();
+        Tiger tiger = new Tiger();
+        Turtle turtle = new Turtle();
+        doSomeAnimalStuff(tiger);
+        doSomeAnimalStuff(animal);
+
+        doSomeAnimalStuffDowncasting(animal);
+        doSomeAnimalStuffDowncasting(turtle);
+    }
+
+    public static void doSomeAnimalStuff(Animal animal) {
+        animal.eat();
+    }
+
+    public static void doSomeAnimalStuffDowncasting(Animal animal) {
+        if(animal instanceof Tiger) {
+            Tiger myLocalTiger = (Tiger) animal;
+            myLocalTiger.eatMyLittleTiger();
+        }
     }
 }
